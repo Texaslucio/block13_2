aaaf7e62bdfcb1e13eeb29aa7c2a60fb78ea1c43
Texaslucio <luciojoseph35@gmail.com>
2023-04-18 07:51:06 -0500

    block13_2

---
+++ greater_than_5/index.js
@@ -0,0 +1,18 @@
+// pseudocode
+
+/*
+    if num1 and num2 are >= 5:
+        - print true
+    else
+        -print false
+*/
+
+// code
+
+let num1 = 5;
+let num2 = 5;
+if (num1 >= 5 && num2 >= 5) {
+    console.log(true);
+} else {
+    console.log(false);
+}
---
+++ is_truthy/index.js
@@ -0,0 +1,28 @@
+// psudocode -- step by step solution a human can understand 
+/*
+    -create a variable called value
+    - if value is truthy, print true 
+    - if value is falsey, then we want to check the follwing conditions:
+    - if value is false
+        - print "The boolean value false is falsy";
+    - if value is null
+        - print "The null value is falsy";
+    ...
+*/
+// code --- step by step instructions that a computer can understand and execute (translating psuedocode into code)
+let value = "";
+if (value) {
+    console.log(true);
+} else {
+    if (value === false) {
+        console.log("The boolean value false is falsy");
+    } else if (value ===null) {
+        console.log("The null value is falsy");
+    } else if (value === undefined) {
+        console.log("undefined is falsy");
+    } else if (value === 0) {
+        console.log("the nuymber 0 is falsy (the only falsy number");
+    } else if (value === "") {
+        console.log("The empty string is falsy (the only falsy string");
+    }
+}
